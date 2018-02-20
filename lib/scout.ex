7321fbc8c8645bfae62f375e6bb5cbd566db135d
Shiraz Butt <shiraz.butt15@imperial.ac.uk>
2018-02-20 01:31:42 +0000

    Style changes

--- lib/scout.ex
+++ lib/scout.ex
@@ -3,29 +3,28 @@
 defmodule Scout do
 
   def start _config, leader, acceptors, ballot_num do
-
     listen leader, acceptors, ballot_num, acceptors, MapSet.new()
-
   end
 
-  def listen leader, acceptors, ballot_num, waitfor, pvalues do
-
+  def listen leader, acceptors, ballot_num, wait_for, pvalues do
     receive do
-      {:p1b, acceptor, bprime, pval} ->
-
-        if bprime == ballot_num do
-
+      { :p1b, acceptor, promised_ballot_num, pval } ->
+        if promised_ballot_num == ballot_num do
            pvalues = MapSet.put(pvalues, pval)
-           waitfor = MapSet.delete(waitfor, acceptor)
+           wait_for = MapSet.delete(wait_for, acceptor)
 
-           if MapSet.size(waitfor) < MapSet.size(acceptors)/2 do
-             send leader, {:adopted, ballot_num, pvalues}
+           if MapSet.size(wait_for) < MapSet.size(acceptors) / 2 do
+             # Once we have a majority, inform the leader.
+             send leader, { :adopted, ballot_num, pvalues }
            else
-             listen leader, acceptors, ballot_num, waitfor, pvalues
+             # Keep receiving responses until we have a majority.
+             listen leader, acceptors, ballot_num, wait_for, pvalues
            end
 
+        # Preempted by higher ballot number, so this ballot may now end in
+        # conflict with another. Tell leader to try again.
         else
-          send leader, {:preempted, bprime}
+          send leader, { :preempted, promised_ballot_num }
         end
     end
   end
