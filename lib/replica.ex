1566da3f49dddddea71e4a7252c8856495d5a4a1
Harry Moore <hrm15@ic.ac.uk>
2018-02-20 18:35:08 +0000

    Completed perform function

--- lib/replica.ex
+++ lib/replica.ex
@@ -17,6 +17,47 @@ defmodule Replica do
       decisions,
       monitor
   ) do
-    
+
+    perform 1, 2, 3, 4
+
   end
+
+  defp perform {_k, _cid, op} = request, slot_out, decisions, database do
+    already_decided? = Enum.any?(
+        decisions,
+        &(value_exists_less_than({slot_out, request}, &1))
+    )
+
+    if already_decided? do
+      slot_out = slot_out + 1
+    else
+
+      send database, {:execute, op}
+
+      slot_out = slot_out + 1
+
+      #send k, {:response, cid, _result}
+
+    end
+
+    { slot_out, database }
+  end
+
+  def propose slot_in, slot_out, window, requests, decisions, proposals, leaders do
+
+    
+
+  end
+
+  defp key_exists key, {newkey, newvalue} do
+
+    key == newkey
+
+  end
+
+
+  defp value_exists_less_than {key, request}, {new_key, new_value} do
+    new_key < key and new_value == request
+  end
+
 end
